64a1178aea35a9f05023c43608d56c512b4fc495
Gurkan Arslan <gurkanarslan1072@gmail.com>
2022-11-10 13:59:55 +0300

    ac1 commit

--- src/test/java/com/cydeo/step_definitions/US_011_Driver_Filter_stepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/US_011_Driver_Filter_stepDefinitions.java
@@ -1,6 +1,6 @@
 package com.cydeo.step_definitions;
 
-import com.cydeo.pages.US_011_Driver_Filter_Page;
+import com.cydeo.pages.DriverFilterPage;
 import com.cydeo.utilities.Driver;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
@@ -9,33 +9,33 @@ import org.openqa.selenium.interactions.Actions;
 public class US_011_Driver_Filter_stepDefinitions {
 
     Actions actions = new Actions(Driver.getDriver());
-   US_011_Driver_Filter_Page us_011_driver_filter_page = new US_011_Driver_Filter_Page();
+    DriverFilterPage driverFilterPage = new DriverFilterPage();
 
 
     @When("The user should see {string} menu")
     public void the_user_should_see_menu(String string) throws InterruptedException {
 
 
-        Thread.sleep(5000);
+        driverFilterPage.waitUntilLoaderScreenDisappear();
         Assert.assertEquals(string,Driver.getDriver().getTitle());
 
     }
     @When("User hover over on fleet menu and click vehicles")
     public void user_hover_over_on_fleet_menu_and_click_vehicles() throws InterruptedException {
-        actions.moveToElement(us_011_driver_filter_page.FleetModule).perform();
-        us_011_driver_filter_page.VehicleButton.click();
-        Thread.sleep(15000);
+        actions.moveToElement(DriverFilterPage.FleetDropdownMenu).perform();
+        DriverFilterPage.VehiclesButton.click();
 
+        driverFilterPage.waitUntilLoaderScreenDisappear();
     }
     @When("User click manage filters button and select driver")
     public void user_click_manage_filters_button_and_select_driver() throws InterruptedException {
-        us_011_driver_filter_page.FilterIconButton.click();
-        Thread.sleep(3000);
-        us_011_driver_filter_page.ManageFilterButton.click();
-        Thread.sleep(3000);
-        us_011_driver_filter_page.DriverCheckBox.click();
-        Thread.sleep(3000);
-        us_011_driver_filter_page.DriverCheckBox.isSelected();
+        DriverFilterPage.FilterIconButton.click();
+        driverFilterPage.waitUntilLoaderScreenDisappear();
+        DriverFilterPage.ManageFilterButton.click();
+        driverFilterPage.waitUntilLoaderScreenDisappear();
+        DriverFilterPage.DriverCheckBox.click();
+        driverFilterPage.waitUntilLoaderScreenDisappear();
+        DriverFilterPage.DriverCheckBox.isSelected();
 
 
     }
---
+++ src/test/resources/features/US_011_Driver_Filter.feature
@@ -0,0 +1,12 @@
+
+@wip
+Feature: US_011_Driver_Filter
+
+  Background:
+    Given The user is on the login page
+
+    Scenario: User can select "Driver" filter under 'Fleet-Vehicles' module
+      When The user logs in as a driver
+      And  The user should see Dashboard
+      And  User hover over on fleet menu and click vehicles
+      Then User click manage filters button and select driver
